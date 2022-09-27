3c4d4be3167fbf6e8e439ac2c5ff49ddd653ce0c
endiawilliams <endiawilliams1@gmail.com>
2022-09-26 22:51:28 -0700

    Initializing Git repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import routes from './config/routes';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      { routes }
+    </div>
+  );
+}
+
+export default App;
